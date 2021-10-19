23eb3eb4268d663f56a2371113b4b9d4345714bc
Sathvick M <92669901+Sathvickm07@users.noreply.github.com>
2021-10-19 09:46:06 +0530

    Update ex.py

--- ex.py
+++ ex.py
@@ -3,3 +3,6 @@ fileName = input("Write the fileName")
 file_extns=fileName.split.(".")
 
 print("the extension is" + repr(file_extns[-1]))
+
+"__repr__ is used to compute the “official” string representation of an object and is typically used for debugging"and
+"If we don’t convert float to string in the above program, join() function will throw exception. Also, we won’t be able to use + operator to concatenate as it will add the floating point numbers"
